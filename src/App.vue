206556fafc04dd019a1f34d5d4d5decc35b94650
Matthew Booth <mvbooth@outlook.com>
2024-01-25 17:01:42 -0600

    initial project commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import MainMenu from './components/shared/MainMenu.vue'
+</script>
+
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar>
+      <MainMenu></MainMenu>
+    </v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item>
+          <v-btn block prepend-icon="mdi-plus" variant="tonal">Block Button</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn block prepend-icon="mdi-plus" variant="tonal">Block Button</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn block prepend-icon="mdi-plus" variant="tonal">Block Button</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn block prepend-icon="mdi-plus" variant="tonal">Block Button</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn block prepend-icon="mdi-plus" variant="tonal">Block Button</v-btn>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <RouterView />
+    </v-main>
+  </v-layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/shared/MainMenu.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <v-toolbar dark>
+        <v-toolbar-title>CryptVue</v-toolbar-title>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-toolbar>
+</template>
+
+<style scoped>
+
+</style>
